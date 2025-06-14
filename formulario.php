b8d9fcea9b452b6159ee8b706948fdc78ed28996
Juan José Vargas <jvargassan@unal.edu.co>
2025-06-14 10:47:40 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=**, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <i>
+        <h1>Hello world</h1>
+    </i>
+    <hr>
+    <br>
+    <hr>
+    <h2>Hello world</h2>
+    <hr>
+    <h3>Hello world</h3>
+    <hr>
+    <h4>Hello world</h4>
+    <hr>
+    <h5>Hello world</h5>
+    <hr>
+    <h6>Hello world</h6>
+
+    <form action="" method="post">
+
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br><br>
+        <input type="submit" value="Submit"> 
+    </form>
+</body>
+
+</html>
